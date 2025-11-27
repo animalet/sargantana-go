a0e042e3e49a46ba185cf62bc3c1cbfca291d7c2
Alberto Segura <asegura@newrelic.com>
2025-11-27 13:52:42 +0100

    feat: add authentication option for static content serving

--- pkg/controller/static.go
+++ pkg/controller/static.go
@@ -15,6 +15,7 @@ type StaticControllerConfig struct {
 	Path string `yaml:"path"`
 	Dir  string `yaml:"dir,omitempty"`
 	File string `yaml:"file,omitempty"`
+	Auth bool   `yaml:"auth,omitempty"` // If true, requires authentication to access static content
 }
 
 func (s StaticControllerConfig) Validate() error {
@@ -50,29 +51,56 @@ func NewStaticController(c *StaticControllerConfig, _ server.ControllerContext) 
 		Str("path", c.Path).
 		Str("dir", c.Dir).
 		Str("file", c.File).
+		Bool("auth", c.Auth).
 		Msg("Static content configured")
 
 	return &static{
-		config: *c,
+		path: c.Path,
+		dir:  c.Dir,
+		file: c.File,
+		auth: c.Auth,
 	}, nil
 }
 
 // static is a controller that serves static files or directories.
 // Each instance handles a single path mapping to either a directory or a file.
+// Fields are extracted from configuration at initialization time for immutability.
 type static struct {
 	server.IController
-	config StaticControllerConfig
+	path string
+	dir  string
+	file string
+	auth bool
 }
 
 // Bind registers the static controller with the provided Gin engine.
 // It sets up routes for serving static files or directories from the configured path.
+// If authentication is enabled, the loginMiddleware is applied to protect the static content.
 func (s *static) Bind(engine *gin.Engine, loginMiddleware gin.HandlerFunc) {
-	if s.config.File != "" {
-		log.Info().Str("path", s.config.Path).Str("file", s.config.File).Msg("Binding static file")
-		engine.StaticFile(s.config.Path, s.config.File)
+	isFile := s.file != ""
+
+	log.Info().
+		Str("path", s.path).
+		Str("file", s.file).
+		Str("dir", s.dir).
+		Bool("auth", s.auth).
+		Msgf("Binding static %s", map[bool]string{true: "file", false: "directory"}[isFile])
+
+	if isFile {
+		if s.auth {
+			engine.GET(s.path, loginMiddleware, func(c *gin.Context) {
+				c.File(s.file)
+			})
+		} else {
+			engine.StaticFile(s.path, s.file)
+		}
 	} else {
-		log.Info().Str("path", s.config.Path).Str("dir", s.config.Dir).Msg("Binding static directory")
-		engine.Static(s.config.Path, s.config.Dir)
+		if s.auth {
+			group := engine.Group(s.path, loginMiddleware)
+			group.Static("/", s.dir)
+		} else {
+			engine.Static(s.path, s.dir)
+		}
 	}
 }
 
